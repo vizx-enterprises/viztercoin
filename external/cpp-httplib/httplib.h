c10ea0dc52d98ee1a924d64fdf89e9d4ae04a6de
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-10-07 18:22:16 +0100

    Windows build fix

--- external/cpp-httplib/httplib.h
+++ external/cpp-httplib/httplib.h
@@ -1771,7 +1771,7 @@ inline socket_t Server::create_server_socket(const char* host, int port, int soc
 {
     return detail::create_socket(host, port,
         [](socket_t sock, struct addrinfo& ai) -> bool {
-            int yes = 1;
+            const char yes = 1;
             if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
                 return false;
             }
