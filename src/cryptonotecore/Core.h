716ed35d16ed6db96bcb0499d44d740d27e694bd
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-11-26 02:26:42 +0000

    Allow syncing with /getrawblocks instead of /getwalletsyncdata

--- src/cryptonotecore/Core.h
+++ src/cryptonotecore/Core.h
@@ -208,6 +208,10 @@ namespace CryptoNote
 
         virtual uint64_t get_current_blockchain_height() const;
 
+        static WalletTypes::RawCoinbaseTransaction getRawCoinbaseTransaction(const CryptoNote::Transaction &t);
+
+        static WalletTypes::RawTransaction getRawTransaction(const std::vector<uint8_t> &rawTX);
+
       private:
         const Currency &currency;
 
@@ -390,10 +394,6 @@ namespace CryptoNote
         void cutSegment(IBlockchainCache &segment, uint32_t startIndex);
 
         void switchMainChainStorage(uint32_t splitBlockIndex, IBlockchainCache &newChain);
-
-        static WalletTypes::RawCoinbaseTransaction getRawCoinbaseTransaction(const CryptoNote::Transaction &t);
-
-        static WalletTypes::RawTransaction getRawTransaction(const std::vector<uint8_t> &rawTX);
     };
 
 } // namespace CryptoNote
